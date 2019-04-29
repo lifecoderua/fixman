1da957c324d88feb49199fc22b92edcd2a0ee3b8
lifecoder <lifecoder.ua@gmail.com>
2019-04-29 22:44:26 +0300

    attach angular ui

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,11 +1,17 @@
+import 'angular-material';
+import 'angular-messages';
 import { NgModule } from 'angular-ts-decorators';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
+import 'angular-material/angular-material.css';
+
 @NgModule({
   id: 'AppModule',
   imports: [
+    'ngMaterial',
+    'ngMessages',
     AppRoutingModule
   ],
   declarations: [
