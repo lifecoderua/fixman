396fcfc5f7dce3e5a3f34233e4c7eced16ec562b
lifecoder <lifecoder.ua@gmail.com>
2019-05-05 08:30:10 +0300

    expand issue model

--- ui/src/app/services/issue.service.ts
+++ ui/src/app/services/issue.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from 'angular-ts-decorators';
 
 const issues = [
-  { id: 1, description: 'stub-description-1', state: 'open', severity: 'medium' },
+  {
+    id: 1, description: 'stub-description-1', state: 'open', severity: 'medium',
+    assignee: { uid: 147, name: 'Contractor Protractor' },
+    operatorAssigned: { uid: 16, name: 'Operator Fox' },
+  },
   { id: 2, description: 'stub-description-2', state: 'en route', severity: 'high' },
   { id: 3, description: 'stub-description-3', state: 'in progress', severity: 'medium' },
-  { id: 4, description: 'stub-description-4', state: 'done', severity: 'low' },
+  {
+    id: 4, description: 'stub-description-4', state: 'done', severity: 'low',
+    assignee: { uid: 16, name: 'Operator Fox' },
+    operatorAssigned: { uid: 16, name: 'Operator Fox' },
+  },
   { id: 5, description: 'stub-description-5', state: 'resolved', severity: 'high' },
   { id: 6, description: 'stub-description-6', state: 'rejected', severity: 'medium' },
   { id: 7, description: 'stub-description-7', state: 'confirmed', severity: 'medium' },
